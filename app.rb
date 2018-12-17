f73e5c394ca8b8bd83ba94e3e314096dbcdc5829
Matthew Cohen <matt.cohen182@gmail.com>
2018-12-17 17:54:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
+    
+    get '/' do
+      erb :'pirates/new'
+    end
+    
+    get '/new' do
+      
+      erb :'pirates/new'
+    end
+    
+    post '/pirates' do
+      erb :'pirates/show'
+    end
 
   end
 end
