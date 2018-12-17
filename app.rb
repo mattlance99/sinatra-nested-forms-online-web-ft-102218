8757382327466337fc66b9532c394930db81a03b
Matthew Cohen <matt.cohen182@gmail.com>
2018-12-17 18:28:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,21 +2,16 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-    
     get '/' do
       erb :root
     end
-    
+
     get '/new' do
       erb :'pirates/new'
     end
-    
+
     post '/pirates' do
-      name = params[:name]
-      weight = params[:weight]
-      height = params[:height]
-      @pirate = Pirate.new(name, weight, height)
-      
+      @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |details|
         Ship.new(details)
@@ -25,6 +20,6 @@ module FormsLab
 
       erb :'pirates/show'
     end
-
+    
   end
 end
